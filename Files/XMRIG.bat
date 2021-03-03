eee3bcc3df7b2634efdf7a55cdaff49d76610da6
ngoc7sao9 <27774949+ngoc7sao9@users.noreply.github.com>
2021-03-03 23:09:49 +0700

    Update XMRIG.bat

--- Files/XMRIG.bat
+++ Files/XMRIG.bat
@@ -1,3 +1,4 @@
+@echo off
 curl -o "C:\Users\Public\Desktop\xmrig.exe" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/xmrig.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\WinRing0x64.sys" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/WinRing0x64.sys > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\SHA256SUMS" https://raw.githubusercontent.com/ngoc7sao9/Windows2019RDP-US/main/Files/SHA256SUMS > out.txt 2>&1
